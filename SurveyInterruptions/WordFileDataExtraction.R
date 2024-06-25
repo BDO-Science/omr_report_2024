811bbe6146744ba29c811558aa08e53d5ff58f52
Bertrand <nbertrand@usbr.gov>
2024-06-25 11:05:59 -0700

    WY2024 update after figures were complete

--- SurveyInterruptions/WordFileDataExtraction.R
+++ SurveyInterruptions/WordFileDataExtraction.R
@@ -1,45 +1,73 @@
-##Code by Nick Bertrand
-#nbertrand@usbr.gov
+#WordFileDataExtraction.R----
 
-#this script will extract the 
-#survey interruptions table from the weekly outlooks from the whole season 
+# Nick Bertrand
+# Start Date: Mon Jun 24 10:24:59 2024
+
+#About----
+#Project: 2024 OMR seasonal Report
+
+#Purpose:#this script will extract the survey interruptions table from the weekly outlooks from the whole season 
 
 #this script also includes the generation of the Sampling disruptions figure.
 #this is the only script needed to generate the figure. 
 
-library(docxtractr)
+#Libraries ----
 library(tidyverse)
+library(readr)
+library(docxtractr)
+library(dplyr)
 
-#########################################
+# Set working directory ----
+#set to location of root object to highest tier directory
+getwd()
+root <- "C:/Users/nbertrand/OneDrive - DOI/Desktop/Bertrand/GitHub/omr_report_2024/SurveyInterruptions"
+setwd(root)
+getwd()
+#these root object use directories 
+data_root<-file.path(root,"Data")
+code_root <- file.path(root,"R_scripts")
+table_output_root <- file.path(root,"Table_Output")
+viz_output_root <- file.path(root,"Viz_Output")
+
+#Export Multiple Files ----
 #A function to iterate through multiple files in a directory
 #creates the dataframe to dump the data in
+#this code section has been glitchy in R studio, it may need to be run, 
+#twice sequentially to generate the data frame.
 survey <- data.frame()
 #reached to the directory with all the Outlook .docx files and creates a list of file names 
-fileNames <- Sys.glob("C:/Users/nbertrand/Desktop/Bertrand/GitHub/OMRSeasonalReport/omr_report_2023/Survey Interruptions/WY2023Outlooks/*.docx")
-#view(fileNames)
+filenames <- Sys.glob(file.path(data_root,"WY2024_Outlooks/*.docx")) #C:/Users/nbertrand/Desktop/Bertrand/GitHub/OMRSeasonalReport/omr_report_2023/Survey Interruptions/WY2023Outlooks/*.docx")
+#filenames <- Sys.glob("C:/Users/nbertrand/OneDrive - DOI/Desktop/Bertrand/GitHub/omr_report_2024/SurveyInterruptions/Data/WY2024_Outlooks/*.docx")
+#view(filenames)
 
+#test <-c("C:/Users/nbertrand/OneDrive - DOI/Desktop/Bertrand/GitHub/omr_report_2024/SurveyInterruptions/Data/WY2024_Outlooks/20231003 fish and water operations outlook.docx")
 #function uses file name list
-for (fileName in fileNames) {
-  
+for ( x in filenames) {
   # to read in the data
-  doc <- read_docx(fileName)
+  doc <- read_docx(x)
   #data is extracted based on table number
-  data1 <- docx_extract_tbl(doc, 8)
-  
-  data2 <- data1 %>% mutate(week = names(data1)[3]) 
-  
-  data2$week <- gsub("Notes..as.of.", "", data2$week)
-  #'.' does not work but for some reason '\\.' does as described by stack overflow
-  data2$week <- gsub('\\.','/',data2$week)
+  #assigns number to all tables by counting them
+  last_num <- docx_tbl_count(doc)
+  #uses the number of the last table in the outlook
+  #number assigned is extracted .
+  data <- docx_extract_tbl(doc, last_num) %>% mutate(week = names(data)[3])
+  #View(data)
+ 
+  data$week <- gsub("Notes..as.of.", "", data$week)
+  #'.' does not work but for some reason '//.' does as described by stack overflow
+  #view(data)
+  data$week <- gsub('.','/',data$week, fixed = TRUE)
+  #view(data)
   #cleans up date formating
-  data2$week <- gsub('/2022/','/2022',data2$week)
-  data2$week <- gsub('/2023/','/2023',data2$week)
+  data$week <- gsub('/2023/','/2023',data$week)
+  data$week <- gsub('/2024/','/2024',data$week)
+  #data$week <- as.Date(data$week, "%m/%d/%y")
   #renames Columns
-  data3 <- data2 %>% rename(Survey = 1, Region = 2, Notes = 3, Status = 4, Week = 5)
+  data3 <- data %>% rename(Survey = 1, Region = 2, Notes = 3, Status = 4, Week = 5)
   #binds extracted data back to the dump dataframe
   survey <- rbind(survey,data3)
 }
-View(survey)
+#View(survey)
 
 #this formating corrects for the lack of zeros in some of the dates
 #survey$Week <- format(as.Date(survey$Week, format = "%m/%d/%Y"), "%d/%m/%Y")
@@ -47,89 +75,108 @@ survey$Week <- format(as.POSIXct(survey$Week, format = "%m/%d/%Y"), "%Y-%m-%d")
 #watch the date formatting and time zone here 
 survey$Week <- as.POSIXct(survey$Week, tz="GMT")
 
-#View(survey)
+View(survey)
 #class(survey$Week)
 #safety check on formatting
 survey$Week
-#' #########################################
-#' #extracts a table from one file, just wanted to save this code
-#' #git hub example for read_docx is broken, code below worked
-#' doc <- read_docx("C:/Users/nbertrand/Desktop/Bertrand/GitHub/OMRSeasonalReport/omr_report_2023/Survey Interruptions/WY2023Outlooks/20230627 fish and water operations outlook.docx")
-#' 
-#' #assigns number to all tables by counting
-#' docx_tbl_count(doc)
-#' #use number assigned in count to extract a specific table.
-#' data <- docx_extract_tbl(doc, 8) %>% 
-#'   mutate(week = names(data)[3]) 
-#' data$week <- gsub("Notes..as.of.", "", data$week)
-#' #'.' does not work but for some reason '\\.' does as described by stack overflow
-#' data$week <- gsub('\\.','/',data$week)
-#' data$week <- as.Date(data$week, "%m/%d/%y")
-#' #renames Columns
-#' data <- data %>%rename(Survey = 1, Region = 2, Notes = 3, Status = 4, Week = 5)
-#' 
-#' view(data)
-#' 
 
-########################
+
+#Extract One outlook File Test code ----
+
+#extracts a table from one file, just wanted to save this code
+#git hub example for read_docx is broken, code below worked
+doc <- read_docx(filenames <- Sys.glob(file.path(data_root,"WY2024_Outlooks/20231003 fish and water operations outlook.docx")))
+
+#assigns number to all tables by counting them
+last_num <- docx_tbl_count(doc)
+#uses the number of the last table in the outlook
+#number assigned is extracted .
+data <- docx_extract_tbl(doc, last_num) %>% mutate(week = names(data)[3])
+#View(data)
+# data2 <- data1 %>% mutate(week = names(data1)[3])
+# View(data2)
+
+data$week <- gsub("Notes..as.of.", "", data$week)
+#'.' does not work but for some reason '//.' does as described by stack overflow
+#view(data)
+data$week <- gsub('.','/',data$week, fixed = TRUE)
+#view(data)
+data$week <- as.Date(data$week, "%m/%d/%y")
+view(data)
+#renames Columns
+data <- data %>%rename(Survey = 1, Region = 2, Notes = 3, Status = 4, Week = 5)
+
+view(data)
+
+
+
+#Gantt Chart Input ----
 #the code below converts the extracted data from the outlooks into a format that can be 
 #read into the gant chart script
 
-########################
-
+##Seven day Start & End date file ----
 #I created a file in excel with the 7 day intervals in which the outlooks are
 #written.
 
 library(readxl)
-WY2023StartandEndDates <- read_excel("Survey Interruptions/WY2023StartandEndDates.xlsx")
-#View(WY2023StartandEndDates)
+StartandEndDates <- read_excel(file.path(data_root,"WY2024StartandEndDates.xlsx"))
+
+#View(StartandEndDates)
 
 #safety check on formatting
 survey$Week
 #safety check on formatting of the dates
-class(WY2023StartandEndDates$Start)
+class(StartandEndDates$Start)
+##Start and end date join ----
 
-gant <- left_join(survey, WY2023StartandEndDates, join_by(Week == Start), keep = TRUE)
+#data frame is joined and some minor edits are done to 
+#format it for the gantt chart input
+gant <- left_join(survey, StartandEndDates, join_by(Week == Start), keep = TRUE)
 
 view(gant)
 
+#adds empty column titled event
+gant$event <- NA
+
 gant_input <- gant %>% 
-  select(Start, End, Survey, Notes, Status) %>% 
-  rename(start=Start, end = End, group = Survey, notes = Notes, category = Status) 
+  select(event, Start, End, Survey, Notes, Status) %>% 
+  rename(start=Start, end = End, group = Survey, notes = Notes, category = Status)  
+#view(gant_input)
 
-
-#minor formatting edits will need to be made in excel 
-#adding an event column with no text except the column header
-
+##Save Input File ----
+#minor formatting edits will need to be made in excel due to oddities in reporting
+#that happen during the season. 
 #this is also a good datafile to create a data check in case outputs look weird later
 
-#write.csv(gant_input, file ="C:/Users/nbertrand/Desktop/Bertrand/GitHub/OMRSeasonalReport/omr_report_2023/Survey Interruptions/WY2023dataExtraction.csv" )
+#commented out to avoid overwriting
+#write.csv(gant_input, file =file.path(data_root, "WY2024dataExtraction.csv"))
 
 
-############
-#GanttInteruptions.R is the script to use from here. 
-
-####################################
-###################
-
+#Re-import file ----
+#This imports the renamed, formatted, and edited file 
 library(readr)
-WY2023dataFormatted <- read_csv("Survey Interruptions/WY2023dataFormatted.csv")
+#make sure the date is formatted in 2024-01-01 in the excel file before import
+WY2024dataFormatted <- read_csv(file.path(data_root,"WY2024dataFormatted.csv"))
 #View(WY2023dataFormatted)
-class(WY2023dataFormatted$category)
 
-#################
+
+#check class of columns if errors are produced below
+#class(WY2023dataFormatted$category)
+
+WY2024dataFormatted$category <- as.numeric(WY2024dataFormatted$category)
+##Import Color Code dataframe ----
 #data frame with color codes to assign to status values (renamed category)
 library(readr)
-ColorCodes <- read_csv("Survey Interruptions/ColorCodes.csv")
-View(ColorCodes)
+ColorCodes <- read_csv(file.path(data_root,"ColorCodes.csv"))
+#View(ColorCodes)
 class(ColorCodes$category)
+##Join color codes----
+#joins the color codes to the formatted datafile
+inter_dat <-left_join(WY2024dataFormatted, ColorCodes, by = join_by("category"))
+view(inter_dat)
 
-inter_dat <-left_join(WY2023dataFormatted, ColorCodes, by = join_by("category"))
-inter_dat
-####################################
-###############
-#######
-
+#Gantt Chart ----
+#libraries
 library(DiagrammeR)
 library(htmlwidgets)
 library(ggplot2)
@@ -140,19 +187,13 @@ library(vistime)
 library("scales")
 library("cowplot")
 
-
-# library(readxl)
-# actions <-  read_csv("C:/Users/nbertrand/Desktop/Bertrand/GitHub/OMRSeasonalReport/omr_report_2023/Survey Interruptions/WY2022_outlookdata6282022format.csv")
-# View(actions)
-
-#actions <- actions %>% select(event,group,start,end,color,category,notes) 
-
+#change object name for this code chunk
 actions <- inter_dat
 
 actions$start <- as.Date(actions$start)
 actions$end <- as.Date(actions$end)
 
-
+##Full Gantt Chart----
 library(plotly)
 d1 <- vistime(actions, optimize_y = T, col.group = "group", col.color = "color", linewidth = 25)
 d1 %>% layout(title = "Disruptions of Sampling",
@@ -161,15 +202,45 @@ d1 %>% layout(title = "Disruptions of Sampling",
 #review the first run of this plot for any weeks missing as 
 #a result of the dates in the outlook table not being correct.
 plot(d1)
-
+d1
 #use the nap shot feature in the full expanded viewer in rstudio to get a .png file
 
-#this is the end of the code that much be used.
+##RST Gantt Chart ----
 
-######################
-#############
-######
-#I have just been resusing the legend already in the port but if not the code below can regnerate.
+rst_list <- c("Red Bluff Diversion Dam Rotary Screw Trap (RST)", 
+              "Knights Landing RST",
+              "Tisdale RST",
+              "GCID RST",
+              "Lower Sacramento RST",
+              "Feather River (upper DWR) RST",
+              "Feather River (lower CDFW) RST",
+              "Caswell RST",
+              "Butte Creek RST/Diversion Trap",
+              "Lower American River at Watt Ave RST",
+              "Yuba River (Hallwood) RST")
+
+rst <- actions %>% filter(group %in% rst_list)
+
+g_rst <- vistime(rst, optimize_y = T, col.group = "group", col.color = "color", linewidth = 25)
+g_rst %>% layout(title = "Disruptions of Sampling",
+              xaxis = list(title = "Date", fixedrange=F, type = 'date', dtick ="M1",tickformat = "%b"))
+
+
+##Survey Gantt Chart----
+
+surv <- actions %>% filter(!group %in% rst_list)
+
+g_surv <- vistime(surv, optimize_y = T, col.group = "group", col.color = "color", linewidth = 25)
+g_surv %>% layout(title = "Disruptions of Sampling",
+                 xaxis = list(title = "Date", fixedrange=F, type = 'date', dtick ="M1",tickformat = "%b"))
+
+
+
+#this is the end of the code that must be used.
+
+#Legend----
+#The code below can create a new legend image if needed.
+#I have just been resusing the legend already in the report but if not the code below can regnerate.
 ### Create a legend image 
 data_legend <- actions %>%
   distinct(category, .keep_all=T) %>%
@@ -212,9 +283,6 @@ plot_combined <- plot_grid(plot, plot_legend,
                            rel_widths = c(1, 0.15))
 plot_combined
 
-
-###################################################################################################################
-library(readxl)
 #actions <- read_excel("WY2021/WY2021_Disruptions.xlsx", 
 #                      sheet = "SaMT")
 #View(WY2021_Disruptions)
